e87f049a0173cf84fe17b8115f29ef08b95d109f
Roger <roger8426@gmail.com>
2026-05-07 03:16:45 +0800

    feat: SpellDto 補 engName / source / classes 欄位

--- src/types/spell.ts
+++ src/types/spell.ts
@@ -1,3 +1,5 @@
+import type { ProfessionKey } from './dnd/profession'
+
 /** 法術學派（以英文 camelCase 為 key，中文僅為顯示 label） */
 export type SpellSchool =
   | 'abjuration'
@@ -9,10 +11,25 @@ export type SpellSchool =
   | 'necromancy'
   | 'transmutation'
 
+/** 法術出處 sourcebook 縮寫 */
+export type SourceKey =
+  | 'AAG'
+  | 'AI'
+  | 'BMT'
+  | 'EGW'
+  | 'FTD'
+  | 'GGR'
+  | 'PHB'
+  | 'SCC'
+  | 'TCE'
+  | 'TDCSR'
+  | 'XGE'
+
 /** 來自 public/json/spells.json 的原始法術資料 */
 export interface SpellDto {
   id: string
   name: string
+  engName: string
   level: number
   school: SpellSchool
   castingTime: string
@@ -23,6 +40,8 @@ export interface SpellDto {
   duration: string
   concentration: boolean
   ritual: boolean
+  source: SourceKey
+  classes: ProfessionKey[]
   desc: string
 }
 
